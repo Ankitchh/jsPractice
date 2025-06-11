ee1f5794ce90afee9a6405bb50930a12c59df8c0
Ankitchh <ankichh1234@gmail.com>
2025-06-11 22:57:42 +0530

    set3 3 question solved 1 remaining

---
+++ set3.js
@@ -0,0 +1,49 @@
+// 1. create a function that takes another function as an argument and calls it after 3 seconds (HOF + Callback)
+
+// let func1 = (arg) => {
+//   setTimeout(arg, 3000);
+// }
+
+// func1(()=>{
+//   console.log("Done")
+// })
+
+
+// 2. Implement your verson of ".map()" as a HOF
+
+// let arr = [1, 2, 3, 4, 5]
+
+
+// let wap = (arr,fun) => {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++){
+//     newArr.push(fun(arr[i]))
+//   }
+//   return newArr
+// }
+
+// var ar = wap(arr, (val) => {
+//   return val + 2
+// })
+
+// console.log(ar);
+
+
+// 3. write a function that uses closures to create a counter.
+
+// function counter() {
+//   let count = 0
+//   return () => {
+//     return ++ count
+//   }
+// }
+
+// const c = counter()
+
+// console.log(c());
+// console.log(c());
+// console.log(c());
+
+
+// 4. Implement a function that limits how many times another function can be called (Closure + HOF)
+
